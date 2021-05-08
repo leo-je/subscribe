e9cdabc603e7718481cee754991e8c7c25712282
哦豁 <leo@qq.com>
2021-05-08 18:44:41 +0800

    commit

--- script/myScript.js
+++ script/myScript.js
@@ -132,29 +132,35 @@ function GetCookie(app) {
 }
 
 function clockinPcBeta(app) {
-    const JDBUrl = {
-        url: 'http://bbs.pcbeta.com/home.php?mod=task&do=apply&id=149',
-        headers: {
-            Cookie: app.read("CookiepcBeta")
-        },
-        body: 'functionId=signBeanIndex&appid=ld'
-    };
-    app.get(JDBUrl, function (error, response, data) {
-        try {
-            if (error) {
-                throw new Error(error)
-            } else {
-                console.log(data);
-                app.notify("pcBeta", "", '签到成功')
+    try {
+        console.log("执行签到")
+        const JDBUrl = {
+            url: 'http://bbs.pcbeta.com/home.php?mod=task&do=apply&id=149',
+            headers: {
+                Cookie: app.read("CookiepcBeta")
+            },
+            body: 'functionId=signBeanIndex&appid=ld'
+        };
+        app.get(JDBUrl, function (error, response, data) {
+            console.log(data)
+            try {
+                if (error) {
+                    throw new Error(error)
+                } else {
+                    app.notify("pcBeta", "", '签到成功')
+                }
+            } catch (eor) {
+                app.notify("pcBeta", "", '签到失败', eor)
             }
-        } catch (eor) {
-            app.notify("pcBeta", "", '签到失败', eor)
-        }
-    })
+        })
+    } catch (e) {
+        console.error(e)
+    }
+
 }
 
 async function start() {
-    console.log("myScript:==================== version 4.4.4 ==================");
+    console.log("myScript:==================== version 4.4.5 ==================");
     var app = applicationContext()
     console.log("isSurge:" + app.isSurge);
     console.log("isRequest:" + app.isRequest);
